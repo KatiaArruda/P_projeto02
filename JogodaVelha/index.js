a10bae49c93a2977f727563a6b3826837acbff47
Katia Arruda | Desenvolvedora Front end <111231609+KatiaArruda@users.noreply.github.com>
2023-10-15 09:09:32 -0300

    ajuste no html e inclusão do js

--- JogodaVelha/index.js
+++ JogodaVelha/index.js
@@ -4,7 +4,7 @@ let turnPlayer = ''
 
 function updateTitle() {
   const playerInput = document.getElementById(turnPlayer)
-  document.getElementById('turnPlayer').innerText = playerInput.value
+  document.getElementById('turnPlayer').inderText = playerInput.value
 }
 
 function initializeGame() {
@@ -20,7 +20,77 @@ function initializeGame() {
     element.classList.remove('win')
     element.innerText = ''
     element.addEventListener('click', handleBoardClick)
+    element.classList.add('cursor-hand')
   })
+}
+
+function disableRegion(element) {
+  element.classList.remove('cursor-hand')
+  element.removeEventListener('click', handleBoardClick)
+}
+
+function getWinRegions() {
+  const WinRegions = []
+  if (
+    vBoard[0][0] &&
+    vBoard[0][0] === vBoard[0][1] &&
+    vBoard[0][0] === vBoard[0][2]
+  )
+    WinRegions.push('0.0', '0.1', '0.2')
+  if (
+    vBoard[1][0] &&
+    vBoard[1][0] === vBoard[1][1] &&
+    vBoard[1][0] === vBoard[1][2]
+  )
+    WinRegions.push('1.0', '1.1', '1.2')
+  if (
+    vBoard[2][0] &&
+    vBoard[2][0] === vBoard[2][1] &&
+    vBoard[2][0] === vBoard[2][2]
+  )
+    WinRegions.push('2.0', '2.1', '2.2')
+  if (
+    vBoard[0][0] &&
+    vBoard[0][0] === vBoard[1][0] &&
+    vBoard[0][0] === vBoard[2][0]
+  )
+    WinRegions.push('0.0', '1.0', '2.0')
+  if (
+    vBoard[0][1] &&
+    vBoard[0][1] === vBoard[1][1] &&
+    vBoard[0][1] === vBoard[2][1]
+  )
+    WinRegions.push('0.1', '1.1', '2.1')
+  if (
+    vBoard[0][2] &&
+    vBoard[0][2] === vBoard[1][2] &&
+    vBoard[0][2] === vBoard[2][2]
+  )
+    WinRegions.push('0.2', '1.2', '2.2')
+  if (
+    vBoard[0][0] &&
+    vBoard[0][0] === vBoard[1][1] &&
+    vBoard[0][0] === vBoard[2][2]
+  )
+    WinRegions.push('0.0', '1.1', '2.2')
+  if (
+    vBoard[0][2] &&
+    vBoard[0][2] === vBoard[1][1] &&
+    vBoard[0][2] === vBoard[2][0]
+  )
+    WinRegions.push('0.2', '1.1', '2.0')
+  return WinRegions
+}
+
+function handleWin(regions) {
+  regions.forEach(function (region) {
+    document
+      .querySelector('[data-region = "' + region + '"]')
+      .classList.add('win')
+  })
+  const playerName = document.getElementById(turnPlayer).value
+  document.querySelector('h2').innerHTML =
+    playerName + ', você' + ' ' + 'venceu!' + '... Parabéns :)'
 }
 
 function handleBoardClick(ev) {
@@ -39,6 +109,16 @@ function handleBoardClick(ev) {
 
   console.clear()
   console.table(vBoard)
+  disableRegion(span)
+  const winRegions = getWinRegions()
+  if (winRegions.length > 0) {
+    handleWin(winRegions)
+  } else if (vBoard.flat().includes('')) {
+    turnPlayer = turnPlayer === 'player1' ? 'player2' : 'player1'
+    updateTitle()
+  } else {
+    document.querySelector('h2').innerHTML = 'Empate!'
+  }
 }
 
 document.getElementById('start').addEventListener('click', initializeGame)
